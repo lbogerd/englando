b1865b6afe70fb10cc44b5a57b733c5cd407b6aa
Lucas Bogerd <lbogerd@protonmail.com>
2018-09-22 15:22:38 +0200

    removed vapp from header

--- components/TopHeader.vue
+++ components/TopHeader.vue
@@ -1,21 +1,19 @@
 <template>
-  <v-app>
-    <v-toolbar 
-      color="primary"
-      fixed
-      app
-    >
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title>Englando</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>
-          <nuxt-link to="/">Home</nuxt-link>
-        </v-btn>
-        <v-btn flat>
-          <nuxt-link to="/pagina">Pagina</nuxt-link>
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-  </v-app>
+  <v-toolbar 
+    color="primary"
+    fixed
+    app
+  >
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>Englando</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>
+        <nuxt-link to="/">Home</nuxt-link>
+      </v-btn>
+      <v-btn flat>
+        <nuxt-link to="/pagina">Pagina</nuxt-link>
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
